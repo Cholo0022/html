874ac276c414b6ba05bf6006ef57912f1a22532f
Cholo0022 <andres.bozzani@gmail.com>
2021-08-31 15:57:53 -0300

    Modulo 5 de javascript

--- 03-js/script.js
+++ 03-js/script.js
@@ -1,1 +1,35 @@
-alert("Hola mundo!");
+let preguntas = [
+    {
+        texto: "Cual es el lugar más frio de la tierra?",
+        opciones: [
+            "Antartida",
+            "Canada",
+            "Rusia",
+            "Argentina"
+        ],
+        respuesta: 0
+    },
+    {
+        texto: "Cual es el río más largo del mundo?",
+        opciones: [
+            "Nilo",
+            "Amazonas",
+            "Uruguay",
+            "Parana"
+        ],
+        respuesta: 1
+    },
+    {
+        texto: "En que continente se encuentra Ecuado",
+        opciones: [
+            "Europa",
+            "Asia",
+            "Oceania",
+            "America"
+        ],
+        respuesta: 3
+    }
+];
+
+let respuestaUsuario = prompt(preguntas[0].texto + "\n" + preguntas[0].opciones[0] + "\n" + preguntas[0].opciones[1] + "\n" + preguntas[0].opciones[2] + "\n" + preguntas[0].opciones[3]);
+
