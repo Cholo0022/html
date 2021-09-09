d4e93bedd279e1fc8fa44abd4d3f55b7092f644c
Cholo0022 <andres.bozzani@gmail.com>
2021-09-09 15:53:59 -0300

    Modulo 6 video 18

--- 04-dom/script.js
+++ 04-dom/script.js
@@ -1,24 +1,31 @@
 const opciones = ["piedra", "papel", "tijera"];
-
+const url = "http://localhost/04-dom/";
 
 function comenzarJuego(e, jugador, maquina) {
     return function () {
+        agregarEvento();
         jugador.classList.remove("clase-animacion");
-        maquina.classList.remove("clase-animacion");
+        maquina.classList.remove("clase-animacion");        
         let opcionComputadora = Math.round(Math.random()*2);
         let opcionUsuario = e.target.id;
+        jugador.src = url + e.target.id + ".png";
+        maquina.src = url + opciones[opcionComputadora] + ".png";
         console.log(chequearOpcion(opcionUsuario, opciones[opcionComputadora]));   
     }
 }
 
 function opcionSelecionada(e) {
+    eliminarEvento();
     let jugador = document.getElementById("jugador");
     let maquina = document.getElementById("maquina");
     jugador.classList.add("clase-animacion");
     maquina.classList.add("clase-animacion");
-    setTimeout(comenzarJuego(e, jugador,maquina), 2100);
+    jugador.src = url + "piedra.png";
+    maquina.src = url + "piedra.png";
+    // Llama a la funcion comenzar juego pasando los tres parametros que necesita, en caso de no necesitar 
+    // parametros se escribiría sin parentesis el "setTimeout(comenzarJuego, 1500);"
+    setTimeout(comenzarJuego(e, jugador,maquina), 1500);
 }
-
 
 function chequearOpcion(opcionUsuario, opcionComputadora){
     if (opcionUsuario === opcionComputadora) return "Empate";
@@ -52,6 +59,16 @@ let piedra = document.getElementById("piedra");
 let papel = document.getElementById("papel");
 let tijera = document.getElementById("tijera");
 
-piedra.addEventListener("click", opcionSelecionada);
-papel.addEventListener("click", opcionSelecionada);
-tijera.addEventListener("click", opcionSelecionada);
+function agregarEvento (){
+    piedra.addEventListener("click", opcionSelecionada);
+    papel.addEventListener("click", opcionSelecionada);
+    tijera.addEventListener("click", opcionSelecionada);
+}
+
+function eliminarEvento (){
+    piedra.removeEventListener("click", opcionSelecionada);
+    papel.removeEventListener("click", opcionSelecionada);
+    tijera.removeEventListener("click", opcionSelecionada);
+}
+
+agregarEvento();
