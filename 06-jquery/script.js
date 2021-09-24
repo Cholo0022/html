49f0837f3adf5f521f71d626baf826c439eb941b
Cholo0022 <andres.bozzani@gmail.com>
2021-09-24 16:18:16 -0300

    Introducción a Jquery y comienzo de proyecto

---
+++ 06-jquery/script.js
@@ -0,0 +1,11 @@
+/*$("#caja").click(function(){
+    $("#caja").css({"background": "blue"});
+});
+
+
+let caja = document.querySelector("#caja");
+caja.addEventListener("click", cambiarColor);
+function cambiarColor() {
+    caja.style.background = "blue";
+}
+*/
