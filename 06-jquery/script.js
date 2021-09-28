e995110d33974593d1a89d97b102df5fdebae874
Cholo0022 <andres.bozzani@gmail.com>
2021-09-28 15:41:58 -0300

    Continuando con la logica del juego Simon

--- 06-jquery/script.js
+++ 06-jquery/script.js
@@ -2,26 +2,51 @@ const opciones = ["verde", "azul", "rojo", "amarillo"];
 let listaJugador = [];
 let listaMaquina = [];
 
-function comenzarJuego() {
-  /*   listaJugador = [];
-    listaMaquina = []; */
-    let aleatorio = Math.floor(Math.random() * 4);
-    let opcion = opciones[aleatorio];
-    $("#" + opcion).addClass(opcion + "-oscuro");
-    agregarOpcionComputadora(opcion);
-    setTimeout(function () {
-        $("#" + opcion).removeClass(opcion + "-oscuro");
-    }, 500)
+function opcionAleatoria() {
+  let aleatorio = Math.floor(Math.random() * 4);
+  let opcion = opciones[aleatorio];
+  return opcion;
 }
 
-function agregarOpcionJugador(e){
-    listaJugador.push(e.target.id);
-    console.log(listaJugador);
+function comenzarJuego() {
+  $(".cuadro").unbind();
+  $(".cuadro").click(chequearOpcion);
+  listaJugador = [];
+  listaMaquina = [];
+  let opcion = opcionAleatoria();
+  agregarOpcion(opcion);
+}
+
+function agregarOpcion(opcion) {
+  listaMaquina.push(opcion);
+  for (let op of listaMaquina) {
+    $("#" + op).addClass(op + "-oscuro");
+    setTimeout(function () {
+      $("#" + op).removeClass(op + "-oscuro");
+    }, 1000);
+  }
+}
+
+function chequearOpcion(e) {
+  listaJugador.push(e.target.id);
+  let indice = listaJugador.length - 1;
+  if (listaJugador[indice] !== listaMaquina[indice]) {
+    perdio();
+    return;
+  }
+  if (listaJugador.length === listaMaquina.length) {
+    listaJugador = [];
+    let opcion = opcionAleatoria();
+    agregarOpcion(opcion);
+  }
+}
+
+function perdio() {
+  $("#puntaje").text(listaMaquina.length - 1);
+  console.log(listaMaquina.length);
+  listaJugador = [];
+  listaMaquina = [];
+  $(".cuadro").unbind();
 }
 
 $("#comenzar").click(comenzarJuego);
-$(".cuadro").click(agregarOpcionJugador);
-function agregarOpcionComputadora(opcion) {
-    listaMaquina.push(opcion);
-    console.log(listaMaquina);    
-}
